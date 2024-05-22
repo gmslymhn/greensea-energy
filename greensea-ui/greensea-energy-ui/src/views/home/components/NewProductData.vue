00a84130a7a089765ab4f0ed4a4934cc4012190c
Leapppppp <1725209246@qq.com>
2024-05-23 06:28:18 +0800

    dsad
    dsadas

---
+++ greensea-ui/greensea-energy-ui/src/views/home/components/NewProductData.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="newProductData"
+ >
+  <span>{{outputValue}}</span>
+</div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import {useTransition} from '@vueuse/core'
+const source = ref(0)
+const outputValue = useTransition(source,{
+  duration:1500
+})
+source.value = 9999
+</script>
+
+<style scoped>
+.newProductData{
+  width: 100%;
+  height: 200px;
+  background-color: #30394b;
+}
+.newProductData{
+  color: white;
+}
+
+</style>
